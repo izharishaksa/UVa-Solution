535657d6105c5bf7e076f87233148edb88804989
Izhari Ishak Aksa <izharishaksa@gmail.com>
2018-12-23 18:28:27 +0700

    ok

--- src/graph/dfs/Problem260.java
+++ src/graph/dfs/Problem260.java
@@ -8,26 +8,6 @@ package graph.dfs;
 public class Problem260 {
 
     public static void main(String[] args) {
-        String s = "a";
-        int i, j, k , len, tot = -1;
-        String r = s;
-        for (i = 0; i < s.length(); i++) {
-            len = 0;
-            for (j = i, k = 0; j < s.length(); j++, k++) {
-                if (s.charAt(j) != r.charAt(k)) break;
-                else len++;
-            }
-            if (len > tot) tot = len;
-        }
-        for (i = tot; i < s.length(); i++) {
-            r += s.charAt(i);
-        }
-        System.out.println(r);
-
-    }
-
-    private static int test() {
-        return 0;
     }
 
 }
